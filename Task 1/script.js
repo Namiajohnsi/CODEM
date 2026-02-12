05e164a4d6560480651c06b25292c3ac38b5842d
Namiajohnsi <160624421+Namiajohnsi@users.noreply.github.com>
2026-02-12 07:40:45 +0530

    Add files via upload

---
+++ Task 1/script.js
@@ -0,0 +1,14 @@
+let x = 12;
+let y = 4;
+
+let add = x + y;
+let sub = x - y;
+let mul = x * y;
+let div = x / y;
+let power = x ** y;
+
+console.log("Addition:", add);
+console.log("Subtraction:", sub);
+console.log("Multiplication:", mul);
+console.log("Division:", div);
+console.log("Power:", power);
