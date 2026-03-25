d2ea2d598853fefc86b2663d4432c5b8a2723721
Namiajohnsi <160624421+Namiajohnsi@users.noreply.github.com>
2026-03-25 23:51:47 +0530

    Add files via upload

---
+++ Node task/app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+
+fs.readFile('users.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.log("Error reading file:", err);
+    return;
+  }
+
+  
+  const users = data.split('\n');
+
+  console.log("User List");
+
+  
+  users.forEach((user, index) => {
+    console.log(`${index + 1}. ${user}`);
+  });
+});
