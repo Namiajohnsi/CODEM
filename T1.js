9990ae0fe3a688ee710ce601022c57c73c6289c9
Namiajohnsi <160624421+Namiajohnsi@users.noreply.github.com>
2026-04-16 19:51:40 +0530

    Add files via upload

---
+++ T1.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to Express Server" });
+});
+
+app.get('/status', (req, res) => {
+    res.json({
+        message: "Server is running",
+        status: "OK"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
